81a10d1a3bbc8612bb46e9a484058dab6dad319a
OMARI1988 <omari.1988@gmail.com>
2016-08-19 11:54:58 +0100

    semantic grammar fixed don't break it !

--- Dukes_dataset/language_and_vision.py
+++ Dukes_dataset/language_and_vision.py
@@ -28,54 +28,51 @@ def _read_vf(scene):
     vf,tree = pickle.load(data)
     return vf,tree
 
-def _read_grammar_trees(scene):
-    pkl_file = '/home/'+getpass.getuser()+'/Datasets_old/Dukes_modified/learning/'+str(scene)+'_grammar.p'
+def _read_semantic_trees(scene):
+    pkl_file = '/home/'+getpass.getuser()+'/Datasets_old/Dukes_modified/learning/'+str(scene)+'_semantic_grammar.p'
     data = open(pkl_file, 'rb')
     tree = pickle.load(data)
     return tree
 
-#--------------------------------------------------------------------------------------------------------#
-# this function tests one susbet of words at a time
-def _all_possibilities_func(subset, hyp_language_pass):
-    all_possibilities = []      # all the possibilities gathered in one list
-    for word in subset:
-        all_possibilities.append(hyp_language_pass[word].keys())
-    return all_possibilities
-
-def _get_semantics(tree, hypotheses_tags):
-    words = []
-    for i in range(len(tree)):
-        for word in tree[i]:
-            words.append(word)
-    semantic_trees = {}
-    all_possibilities = _all_possibilities_func(words, hypotheses_tags)
-    for count,element in enumerate(itertools.product(*all_possibilities)):
+def _is_valid_action_query(tree):
+    action_valid = 0
+    query_valid = 0     # make sure every query is valid
+    for query in tree:
         actions = 0
-        for e in element:
-            if 'actions_' in e:
+        for item in query:
+            if 'actions_' in item:
                 actions+=1
-        if actions == 1:
-            semantic_trees[count] = deepcopy(tree)
-            c = 0
-            for i in range(len(semantic_trees[count])):
-                for j in range(len(semantic_trees[count][i])):
-                    semantic_trees[count][i][j] = element[c]
-                    c += 1
-    return semantic_trees
+        if actions==0:
+            query_valid+=1
+        if actions>0 and len(query)==actions:
+            query_valid+=1
+    if query_valid == len(query):
+        action_valid=1
+    return action_valid
+
+
+def _is_valid_query(tree):
+    action_valid = _is_valid_action_query(tree)
+    # print tree
+    if action_valid:
+        print tree
+
+
+def _validate(tree):
+    valid = _is_valid_query(tree)
 
 hypotheses_tags, VF_dict, LF_dict = _read_tags()
-for scene in range(1,1001):
+for scene in range(17,18):
     semantic_trees = {}
-    print 'generating grammar from scene : ',scene
+    print 'test grammar from scene : ',scene
     VF,Tree = _read_vf(scene)
     sentences = _read_sentences(scene)
-    grammar_trees = _read_grammar_trees(scene)
+    semantic_trees = _read_semantic_trees(scene)
     counter = 0
-    for id in sentences:
-        semantic_trees[id] = {}
-        for t in grammar_trees[id]:
-            tree = grammar_trees[id][t]
-            semantic_trees[id][t] = _get_semantics(tree,hypotheses_tags)
-            counter += len(semantic_trees[id][t])
-    pkl_file = '/home/'+getpass.getuser()+'/Datasets_old/Dukes_modified/learning/'+str(scene)+'_semantic_grammar.p'
-    pickle.dump(semantic_trees, open(pkl_file, 'wb'))
+    for id in semantic_trees:
+        print id
+        for grammar in semantic_trees[id]:
+            for semantic in semantic_trees[id][grammar]:
+                tree = semantic_trees[id][grammar][semantic]
+                _validate(tree)
+            # counter += len(semantic_trees[id][t])
--- Dukes_dataset/semantic_grammar_generation.py
+++ Dukes_dataset/semantic_grammar_generation.py
@@ -34,12 +34,31 @@ def _read_grammar_trees(scene):
     tree = pickle.load(data)
     return tree
 
+def _test_Actions(element):
+    action_flag = 1
+    action_position = []
+    for count,item in enumerate(element):
+        if 'actions_' in item:
+            action_position.append(count)
+    if len(action_position) == 0:
+        action_flag = 0
+        # print 'test1'
+    elif len(action_position) > 1:
+        L = [a_i - b_i for a_i, b_i in zip(action_position[:-1],action_position[1:])]
+        for i in L:
+            if i!= -1:
+                action_flag=0
+                # print 'test2'
+                # print action_position
+    # if action_flag:
+    #     print element
+    return action_flag
+
 #--------------------------------------------------------------------------------------------------------#
 # this function tests one susbet of words at a time
 def _all_possibilities_func(subset, hyp_language_pass):
     all_possibilities = []      # all the possibilities gathered in one list
     for word in subset:
-        # print word,hyp_language_pass[word].keys()
         all_possibilities.append(hyp_language_pass[word].keys())
     return all_possibilities
 
@@ -51,17 +70,19 @@ def _get_semantics(tree, hypotheses_tags):
     semantic_trees = {}
     all_possibilities = _all_possibilities_func(words, hypotheses_tags)
     for count,element in enumerate(itertools.product(*all_possibilities)):
-        semantic_trees[count] = deepcopy(tree)
-        c = 0
-        for i in range(len(semantic_trees[count])):
-            for j in range(len(semantic_trees[count][i])):
-                semantic_trees[count][i][j] = element[c]
-                c += 1
-        print semantic_trees[count]
+        valid = _test_Actions(element)
+        if valid:
+            semantic_trees[count] = deepcopy(tree)
+            c = 0
+            for i in range(len(semantic_trees[count])):
+                for j in range(len(semantic_trees[count][i])):
+                    semantic_trees[count][i][j] = element[c]
+                    c += 1
+            # print semantic_trees[count]
     return semantic_trees
 
 hypotheses_tags, VF_dict, LF_dict = _read_tags()
-for scene in range(1,2):
+for scene in range(1,1001):
     semantic_trees = {}
     print 'generating grammar from scene : ',scene
     VF,Tree = _read_vf(scene)
@@ -69,12 +90,13 @@ for scene in range(1,2):
     grammar_trees = _read_grammar_trees(scene)
     counter = 0
     for id in sentences:
+        # print '###############################################################'
+        # print sentences[id]['text']
+        # print '###############################################################'
         semantic_trees[id] = {}
         for t in grammar_trees[id]:
             tree = grammar_trees[id][t]
             semantic_trees[id][t] = _get_semantics(tree,hypotheses_tags)
             counter += len(semantic_trees[id][t])
-    print tree
-    print counter
     pkl_file = '/home/'+getpass.getuser()+'/Datasets_old/Dukes_modified/learning/'+str(scene)+'_semantic_grammar.p'
     pickle.dump(semantic_trees, open(pkl_file, 'wb'))
