7d9588dce25e4c820ddec461651eda15ee9baeba
OMARI1988 <omari.1988@gmail.com>
2016-09-14 08:31:40 +0100

    updates

--- Results_plotting/similarity_matrix.py
+++ Results_plotting/similarity_matrix.py
@@ -1,11 +1,40 @@
-import numpy
+import numpy as np
 import matplotlib.pylab as plt
+from matplotlib.font_manager import FontProperties
+# import matplotlib.pyplot as plt
+
+font = FontProperties()
+families = ['serif', 'sans-serif', 'cursive', 'fantasy', 'monospace']
+font.set_family(families[0])
+
+ax = plt.subplot(1, 3, 1)
+matrix = np.random.rand(7,7)
+plt.imshow(matrix, interpolation='nearest', cmap=plt.get_cmap('inferno'), extent=(0,7,0,7))
+# plt.title('Real-world dataset', fontsize=20)
+# plt.text(0.5, -.1, 'Real-world dataset',
+#          horizontalalignment='center',
+#          fontsize=20,
+#          transform = ax.transAxes)
+plt.xticks(np.arange(7)+.6, ['colour','shape','location','direction','distance','action','function'], fontsize=20, rotation=80)
+ax.xaxis.tick_top()
+plt.yticks(np.arange(7)+.5, reversed(['colour','shape','location','direction','distance','action','function']), fontsize=20)
 
 
-m = [[0.0, 1.47, 2.43, 3.44, 1.08, 2.83, 1.08, 2.13, 2.11, 3.7], [1.47, 0.0, 1.5,     2.39, 2.11, 2.4, 2.11, 1.1, 1.1, 3.21], [2.43, 1.5, 0.0, 1.22, 2.69, 1.33, 3.39, 2.15, 2.12, 1.87], [3.44, 2.39, 1.22, 0.0, 3.45, 2.22, 4.34, 2.54, 3.04, 2.28], [1.08, 2.11, 2.69, 3.45, 0.0, 3.13, 1.76, 2.46, 3.02, 3.85], [2.83, 2.4, 1.33, 2.22, 3.13, 0.0, 3.83, 3.32, 2.73, 0.95], [1.08, 2.11, 3.39, 4.34, 1.76, 3.83, 0.0, 2.47, 2.44, 4.74], [2.13, 1.1, 2.15, 2.54, 2.46, 3.32, 2.47, 0.0, 1.78, 4.01], [2.11, 1.1, 2.12, 3.04, 3.02, 2.73, 2.44, 1.78, 0.0, 3.57], [3.7, 3.21, 1.87, 2.28, 3.85, 0.95, 4.74, 4.01, 3.57, 0.0]]
+ax = plt.subplot(1, 3, 2)
+matrix = np.random.rand(7,7)
+plt.imshow(matrix, interpolation='nearest', cmap=plt.get_cmap('inferno'), extent=(0,7,0,7))
+# plt.xticks(np.arange(7)+.6, ['colour','shape','location','direction','distance','action','function'], fontsize=20, rotation=80)
+ax.xaxis.tick_top()
+# plt.yticks(np.arange(7)+.5, reversed(['colour','shape','location','direction','distance','action','function']), fontsize=20)
 
-matrix = numpy.matrix(m)
 
-plt.imshow(matrix, interpolation='nearest', cmap=plt.cm.jet, extent=(0.5,10.5,0.5,10.5))
-plt.colorbar()
+ax = plt.subplot(1, 3, 3)
+matrix = np.random.rand(7,7)
+plt.imshow(matrix, interpolation='nearest', cmap=plt.get_cmap('inferno'), extent=(0,7,0,7))
+# plt.xticks(np.arange(7)+.6, ['colour','shape','location','direction','distance','action','function'], fontsize=20, rotation=80)
+ax.xaxis.tick_top()
+# plt.yticks(np.arange(7)+.5, reversed(['colour','shape','location','direction','distance','action','function']), fontsize=20)
+
+
+# plt.colorbar()
 plt.show()
