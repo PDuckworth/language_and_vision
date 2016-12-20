0172a9e9a987aab4ad3228ae333ef0e5dd6e1fce
OMARI1988 <omari.1988@gmail.com>
2016-12-20 18:32:16 +0000

    extracting colour features from ECAI dataset

--- nice_plotting/group_images.py
+++ nice_plotting/group_images.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cv2
 
-dir1 = '/home/omari/Dropbox/Reports/AAAI17/camera_ready/pics/'
+dir1 = '/home/omari/Dropbox/Reports/AAAI17/not_used/'
 # print img.shape
 sp = 20
-total_img = np.zeros((806,480*4+sp*3,3),dtype=np.uint8)+255
+total_img = np.zeros((806,480*5+sp*4,3),dtype=np.uint8)+255
 c = 0
-for i in [1,2,4,5]:
+for i in [1,2,3,4,5]:
+    print i
     img = cv2.imread(dir1+'IMG_'+str(i)+'.jpg')
     img = cv2.resize(img,None,fx=.2, fy=.2, interpolation = cv2.INTER_CUBIC)
     img = img[:,75:555,:]
@@ -16,5 +17,5 @@ for i in [1,2,4,5]:
 
 total_img = total_img[50:,:,:]
 cv2.imshow('img',total_img)
-cv2.imwrite(dir1+'example4.png',total_img)
+cv2.imwrite(dir1+'example5.png',total_img)
 cv2.waitKey(2000)
