dde7e8e938d6926a28d80eb6931eb908a398aa99
OMARI1988 <omari.1988@gmail.com>
2017-06-22 13:00:53 +0100

    counting unique words in all commands

--- Baxter_dataset/3-language.py
+++ Baxter_dataset/3-language.py
@@ -17,11 +17,12 @@ class language():
         self.unique_words = []
 
     def _read_sentences(self):
-        for video in range(1,51):
+        for video in range(1,205):
             dir1 = self.dir+str(video)+"/annotation/natural_language_commands.txt"
             f = open(dir1,'r')
             for line in f:
                 line = line.split("\n")[0]
+                print video,line
                 words = line.split(" ")
                 for word in words:
                     if word not in self.unique_words:
