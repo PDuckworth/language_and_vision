0c6567aaac22a661185e61c8f9d7d4a8056561d7
OMARI1988 <omari.1988@gmail.com>
2017-06-22 10:38:42 +0100

    counting the total number of words in this dataset

--- Dukes_dataset/all_images.py
+++ Dukes_dataset/all_images.py
@@ -1,8 +1,10 @@
 import cv2
+import numpy as np
 
 count = 0
-for i in range(20,300):
-    for j in ["0000","0004","0008","0012","0015"]:
+for i in range(1,10):
+    img_final = np.zeros((10,10,3),dtype=np.uint8)
+    for j in ["0000","0009","0014","0018","0023"]:
         if i < 10:
             dir1 = "/home/omari/Datasets/robot_modified/scenes/"+str(i)+"/scene_000"+str(i)+"_frame_"+j+".png"
         elif i < 100:
@@ -13,8 +15,18 @@ for i in range(20,300):
             dir1 = "/home/omari/Datasets/robot_modified/scenes/"+str(i)+"/scene_"+str(i)+"_frame_"+j+".png"
         print dir1
         img = cv2.imread(dir1)
-        img = img[:-200,100:-150,:]
-        cv2.imwrite("/home/omari/Datasets/robot_modified/all_images/robot_"+str(count)+".png",img)
-        count+=1
-        # cv2.imshow("img",img)
-        # cv2.waitKey(200)
+        try:
+            img = img[100:-150,100:-300,:]
+            if j == "0000":
+                img_final = img
+            else:
+                img_final = np.concatenate((img_final, img), axis=1)
+        except Exception as e:
+            # cv2.imwrite("/home/omari/Datasets/robot_modified/all_images/robot_"+str(count)+".png",img_final)
+            # count+=1
+            print "wrong scene"
+
+    cv2.imwrite("/home/omari/Datasets/robot_modified/all_images/robot_"+str(i)+".png",img_final)
+    # count+=1
+    # cv2.imshow("img",img_final)
+    # cv2.waitKey(200)
