70db8326fdf9a279342633c1247481f640df5240
OMARI1988 <omari.1988@gmail.com>
2017-07-17 16:30:45 +0100

    image in thesis

--- jivko_dataset/thesis_v_concept_image.py
+++ jivko_dataset/thesis_v_concept_image.py
@@ -10,8 +10,8 @@ colors = '/home/omari/Datasets/jivko_dataset/features/colours/'
 #objects = '/home/omari/Datasets/ECAI_dataset/objects/'
 image_dir = "/home/omari/Dropbox/Thesis/writing/Chapter7/Chapter7Figs/PNG/sinapov-visual-concepts.jpg"
 
-shape_im = ['9_cluster', '3_cluster']
-color_im = ['2_cluster', '3_cluster']
+shape_im = ['9_cluster', '3_cluster', '1_cluster', '8_cluster']
+color_im = ['2_cluster', '3_cluster', '5_cluster', '7_cluster']
 # location_im = ['2_cluster', '5_cluster']
 # distance_im = ['0_cluster', '2_cluster']
 all_images = [shape_im, color_im] #, location_im, distance_im]
@@ -22,7 +22,7 @@ im_len = 60
 th = 40
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-image = np.zeros((im_len*5*2+th*2,im_len*5*3+th*2,3),dtype=np.uint8)+255
+image = np.zeros((im_len*5*2+th*2,im_len*5*4+th*3,3),dtype=np.uint8)+255
 
 for c1,i in enumerate(all_images):
     dir_ = all_dir[c1]
@@ -30,8 +30,8 @@ for c1,i in enumerate(all_images):
     for c2,j in enumerate(i):
         im = cv2.imread(dir_+j+".jpg")
         print dir_+j+".jpg"
-        image[c2*(im_len*5)+c2*th:(c2+1)*(im_len*5)+c2*th, c1*(im_len*5)+c1*th:(c1+1)*(im_len*5)+c1*th, :] = im
-        cv2.putText(image,t+'_'+str(c2),   (100+c1*(im_len*5)+c1*th, (c2+1)*(im_len*5)+(c2)*th+th*1/2),    font, .8,(0,0,0),2)
+        image[c1*(im_len*5)+c1*th:(c1+1)*(im_len*5)+c1*th, c2*(im_len*5)+c2*th:(c2+1)*(im_len*5)+c2*th, :] = im
+        cv2.putText(image,t+'_'+str(c2),   (100+c2*(im_len*5)+c2*th, (c1+1)*(im_len*5)+(c1)*th+th*1/2),    font, .8,(0,0,0),2)
         # print t
 cv2.imwrite(image_dir,image)
 cv2.imshow("test",image)
